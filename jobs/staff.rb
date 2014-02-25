20c589625a363b0259988708b134d1755a80cc06
Isklar <mrisklar@gmail.com>
2014-02-25 01:26:39 +0000

    Added open-uri error checking

--- jobs/staff.rb
+++ jobs/staff.rb
@@ -18,53 +18,54 @@ prisonUrl = "http://localhost:20071/api/2/call?json=%5B%7B%22name%22%3A%22" + ap
 skyblockUrl = "http://localhost:20074/api/2/call?json=%5B%7B%22name%22%3A%22" + apimethod + "%22%2C%22key%22%3A%22" + sha256.to_s + "%22%2C%22username%22%3A%22" + username + "%22%2C%22arguments%22%3A%5B%5D%2C%22tag%22%3A%22Skyblockstaff%22%7D%5D"
 skywarsUrl = "http://localhost:20077/api/2/call?json=%5B%7B%22name%22%3A%22" + apimethod + "%22%2C%22key%22%3A%22" + sha256.to_s + "%22%2C%22username%22%3A%22" + username + "%22%2C%22arguments%22%3A%5B%5D%2C%22tag%22%3A%22Skywarsstaff%22%7D%5D"
 
+urls = Array.new
+urls.push factionsUrl
+urls.push freebuildUrl
+urls.push hubUrl
+urls.push kitUrl
+urls.push prisonUrl
+urls.push skyblockUrl
+urls.push skywarsUrl
+
 # Generate empty hash to store online staff and server locations
 staff_locs = Hash.new({ value: 0 })
+
 # Using an array instead of fetching from JSONAPI because the group getter method crashes the server
 staff_list = ["bsquidwrd","BigRichieRich","EthanGrievous","Grimdeathkin","Isklar","irRedemption","MiniBuscus","snowboi","The_Smarty_Cat","ThunderWizard","Will124"]
+serverPlayers = Array.new
+serverNames = ["Factions", "Freebuild", "Hub", "KitPvP", "Prison", "Skyblock", "Skywars"]
 
 SCHEDULER.every '15s' do
-
-  factionsResponse = JSON.parse(open(factionsUrl).read)[0]
-  factionsList = factionsResponse.fetch("success")
-
-  freebuildResponse = JSON.parse(open(freebuildUrl).read)[0]
-  freebuildList = freebuildResponse.fetch("success")
-
-  hubResponse = JSON.parse(open(hubUrl).read)[0]
-  hubList = hubResponse.fetch("success")
-
-  kitResponse = JSON.parse(open(kitUrl).read)[0]
-  kitList = kitResponse.fetch("success")
-
-  prisonResponse = JSON.parse(open(prisonUrl).read)[0]
-  prisonList = prisonResponse.fetch("success")
-
-  skyblockResponse = JSON.parse(open(skyblockUrl).read)[0]
-  skyblockList = skyblockResponse.fetch("success")
-
-  skywarsResponse = JSON.parse(open(skywarsUrl).read)[0]
-  skywarsList = skywarsResponse.fetch("success")
-
-  staff_list.each { |name|
-   if factionsList.include?(name)
-     staff_locs[name] = { label: name, value: 'Factions'}
-   elsif freebuildList.include?(name)
-     staff_locs[name] = { label: name, value: 'Freebuild'}
-   elsif hubList.include?(name)
-     staff_locs[name] = { label: name, value: 'Hub'}
-   elsif kitList.include?(name)
-     staff_locs[name] = { label: name, value: 'KitPvP'}
-   elsif prisonList.include?(name)
-     staff_locs[name] = { label: name, value: 'Prison'}
-   elsif skyblockList.include?(name)
-     staff_locs[name] = { label: name, value: 'Skyblock'}
-   elsif skywarsList.include?(name)
-     staff_locs[name] = { label: name, value: 'Skywars'}
-   else
-     staff_locs.delete(name)
+ x = 0
+  urls.each do |url|
+   begin
+      urlResponse = open(url).read
+    rescue Timeout::Error
+      puts "The request for a page at #{url} timed out...skipping."
+    rescue OpenURI::HTTPError => e
+      puts "The request for a page at #{url} returned an error. #{e.message}"
+    rescue Errno::ECONNREFUSED
+      puts "The request for a page at #{url} refused the connection...skipping."
+    else
+      urlJson = JSON.parse(urlResponse)[0]
+      serverPlayers[x] = urlJson.fetch("success")
+      staff_list.each { |name|
+        if serverPlayers[x].include?(name)
+           staff_locs[name] = { label: name, value: serverNames[x]}
+        end
+      }
+    ensure
+      x += 1
    end
-  }
+  end
+
+allPlayers = serverPlayers[0..6].flatten
+  staff_list.each { |name|
+     if !allPlayers.include?(name)
+       staff_locs.delete(name)
+    end
+  }  
+
 
   if staff_locs.length == 0
     staff_locs['Nobody'] = { label: 'Nobody', value: ':('}
@@ -77,6 +78,5 @@ SCHEDULER.every '15s' do
   end
 
   send_event('staff', { items: staff_locs.values })
-#  send_event('debug', { text: FBonlineList })
 end
 
